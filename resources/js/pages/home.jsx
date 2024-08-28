cae09a4b9362d5b250d045fcc83e88f886c43ec2
Danan <narayanmade578@gmail.com>
2024-08-28 19:39:18 +0700

    feat: setup Inertia.js, Vite, and Tailwind CSS for Laravel project

---
+++ resources/js/pages/home.jsx
@@ -0,0 +1,9 @@
+export default function home() {
+    return (
+        <>
+            <div className={`text-center bg-blue-700`}>
+                <h1 className={`font-bold text-red-400 `}>Homepage UK</h1>
+            </div>
+        </>
+    )
+}
